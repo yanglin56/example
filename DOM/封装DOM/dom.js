98bfe632e9198430dd87da72f468b0936512aac4
yanglin <yanglin556@yahoo.com>
2022-05-21 23:44:01 +0800

    更新下页面

--- DOM/封装DOM/dom.js
+++ DOM/封装DOM/dom.js
@@ -1,16 +1,25 @@
 window.dom = {
-    create(string) {
-        const container = document.createElement('template')
-        container.innerHTML = string.trim()
-        return container.content.firstChild
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector);
     },
-    find() {
-
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value;
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                //dom.style(div,'color')
+                return node.style[name];
+            } else if (name instanceof Object) {
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key];
+                }
+            }
+        }
     },
-    style() {
-
-    },
-    each() {
-
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i]);
+        }
     }
-}
+};
