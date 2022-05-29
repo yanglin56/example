f7b909c27403794de67f2db4fbd41d2d6ce523a0
yanglin <yanglin556@yahoo.com>
2022-05-29 23:36:13 +0800

    上传新增内容

--- ajax-1/public/main.js
+++ ajax-1/public/main.js
@@ -1,3 +1,29 @@
+getXML.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/4.xml');
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const dom = request.responseXML;
+            const text = dom.getElementsByClassName('warning')[0].textContent
+            console.log(text.trim())
+        }
+    };
+    request.send();
+};
+getHTML.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/3.html');
+    request.onload = () => {
+        //创建 div 标签
+        const div = document.createElement('div');
+        //填写 div 内容
+        div.innerHTML = request.response
+            //插入到身体里
+        document.body.appendChild(div)
+    };
+    request.onerror = () => {};
+    request.send();
+};
 getJS.onclick = () => {
     const request = new XMLHttpRequest();
     request.open('GET', '/2.js');
@@ -9,24 +35,31 @@ getJS.onclick = () => {
         script.innerHTML = request.response
             //插到身体里
         document.body.appendChild(script)
-    }
-    request.onerror = () => {}
-    request.send()
-}
+    };
+    request.onerror = () => {};
+    request.send();
+};
 getCSS.onclick = () => {
     const request = new XMLHttpRequest();
-    request.open('GET', '/style.css');
-    request.onload = () => {
+    request.open('GET', '/style.css'); //readyState = 1
+    request.onreadystatechange = () => {
+        console.log(request.readyState)
+        if (request.readyState === 4) {
+            //下载完成。但不知道是成功 2xx 还是失败 4xx 5xx
+            if (request.status >= 200 && request.status < 300) {
+                //创建 style 标签
+                const style = document.createElement('style')
+                    //填写 style 内容
+                style.innerHTML = request.response
+                    //插到头里面
+                document.head.appendChild(style)
+            } else {
+                alert('加载 CSS 失败')
+            }
 
-        //创建 style 标签
-        const style = document.createElement('style')
-            //填写 style 内容
-        style.innerHTML = request.response
-            //插到头里面
-        document.head.appendChild(style)
+
+        }
+
     };
-    request.onerror = () => {
-        console.log('失败了');
-    };
-    request.send();
+    request.send(); //readyState = 2
 };
--- ajax-1/server.js
+++ ajax-1/server.js
@@ -42,6 +42,16 @@ var server = http.createServer(function(request, response) {
         response.setHeader('Content-Type', 'text/script;charset=utf-8')
         response.write(fs.readFileSync('public/2.js'))
         response.end()
+    } else if (path === '/3.html') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(fs.readFileSync('public/3.html'))
+        response.end()
+    } else if (path === '/4.xml') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+        response.write(fs.readFileSync('public/4.xml'))
+        response.end()
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
