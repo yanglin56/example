7933d06bc11d3980f727cf147b5a761c62cc953a
yanglin <yanglin556@yahoo.com>
2022-06-03 22:29:49 +0800

    init

--- kuayu-1/frank-com/public/frank.js
+++ kuayu-1/frank-com/public/frank.js
@@ -1,22 +1,8 @@
-function jsonp(url) {
-    return new Promise((resolve, reject) => {
-        const random = "frankJSONPCallbackName" + Math.random();
-        window[random] = (data) => {
-            resolve(data);
-        };
-        const script = document.createElement("script");
-        script.src = `${url}?callback=${random}`;
-        script.onload = () => {
-            script.remove();
-        };
-        script.onerror = () => {
-            reject();
-        };
-        document.body.appendChild(script);
-    });
+const request = new XMLHttpRequest()
+request.open('GET', ' https://qq.com:8888/friends.json')
+request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.response)
+    }
 }
-
-jsonp('http://qq.com:8888/friends.js')
-    .then((data) => {
-        console.log(data)
-    })
+request.send()
