fd705711546bee128f7dd6b11d19c806bb4d2caa
yanglin <yanglin556@yahoo.com>
2022-06-04 23:01:51 +0800

    init

--- static-server-2/server.js
+++ static-server-2/server.js
@@ -21,28 +21,34 @@ var server = http.createServer(function(request, response) {
 
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    if (path === '/index.html') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(fs.readFileSync('./public/index.html'))
-        response.end()
-    } else if (path === '/qq.js') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-        response.write(fs.readFileSync('./public/qq.js'))
-        response.end()
-    } else if (path === '/friends.json') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/json;charset=utf-8')
-        response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:9990')
-        response.write(fs.readFileSync('./public/friends.json'))
-        response.end()
-    } else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
-        response.end()
+    response.statusCode = 200
+        //默认首页
+    const filePath = path === '/' ? '/index.html' : path
+    const index = filePath.lastIndexOf('.')
+        //suffix 是后缀
+    const suffix = filePath.substring(index);
+    const fileType = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "text/javascript",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+    };
+    console.log(fileType[suffix]);
+    response.setHeader(
+        "Content-Type",
+        `${fileType[suffix] || "text/html"};charset=utf-8`
+    );
+    let content;
+    try {
+        content = fs.readFileSync(`./public${filePath}`);
+    } catch (error) {
+        response.statusCode = 404;
+        content = "404";
     }
+
+    response.write(content);
+    response.end();
 
     /******** 代码结束，下面不要看 ************/
 })
