20cee05114035d2a43e3326c13aa7575e911ee47
yanglin <yanglin556@yahoo.com>
2022-06-07 23:51:08 +0800

    init

--- kuayu-1/qq-com/server.js
+++ kuayu-1/qq-com/server.js
@@ -21,15 +21,22 @@ var server = http.createServer(function(request, response) {
 
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    if (path === '/') {
+    if (path === '/index.html') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`qq.com`)
+        response.write(fs.readFileSync('./public/index.html'))
         response.end()
-    } else if (path === '/x') {
+    } else if (path === '/qq.js') {
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.write(fs.readFileSync('./public/qq.js'))
+        response.end()
+    } else if (path === '/friends.json') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        console.log(request.headers['referer'])
+        response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:9999')
+        response.write(fs.readFileSync('./public/friends.json'))
         response.end()
     } else {
         response.statusCode = 404
