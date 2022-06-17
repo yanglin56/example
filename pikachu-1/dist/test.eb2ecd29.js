0aa8365f6bdb4c715a5a2492aec82ee72bd199fb
yanglin <yanglin556@yahoo.com>
2022-06-17 21:06:18 +0800

    init

--- pikachu-1/dist/test.eb2ecd29.js
+++ pikachu-1/dist/test.eb2ecd29.js
@@ -103,13 +103,88 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({"test.js":[function(require,module,exports) {
-var string = "\n* {\n    box-sizing: border-box;\n    margin: 0;\n    padding: 0;\n}\n\n* ::before,\n* ::after {\n    box-sizing: border-box;\n}\n\nbody {\n    background: #ffe600;\n    min-height: 100vh;\n}\n\n.skin {\n    position: relative;\n}\n\n.nose {\n    border: 10px solid black;\n    border-color: black transparent transparent;\n    border-bottom: none;\n    width: 0px;\n    height: 0px;\n    position: relative;\n    left: 50%;\n    top: 145px;\n    margin-left: -10px;\n    z-index: 10;\n}\n\n@keyframes wave {\n    0% {\n        transform: rotate(0deg);\n    }\n    33% {\n        transform: rotate(5deg);\n    }\n    66% {\n        transform: rotate(-5deg);\n    }\n    100% {\n        transform: rotate(0deg);\n    }\n}\n\n.nose:hover {\n    transform-origin: center bottom;\n    animation: wave 300ms infinite linear;\n}\n\n.yuan {\n    position: absolute;\n    width: 20px;\n    height: 6px;\n    top: -16px;\n    left: -10px;\n    border-radius: 10px 10px 0 0;\n    background: black;\n}\n\n.eye {\n    border: 2px solid #000;\n    width: 64px;\n    height: 64px;\n    position: absolute;\n    left: 50%;\n    top: 100px;\n    margin-left: -32px;\n    background: #2e2e2e;\n    border-radius: 50%;\n}\n\n.eye::before {\n    content: '';\n    display: block;\n    border: 3px solid #000;\n    width: 30px;\n    height: 30px;\n    background: #fff;\n    border-radius: 50%;\n    position: relative;\n    left: 4px;\n    top: 2px;\n}\n\n.eye.left {\n    transform: translateX(-100px);\n}\n\n.eye.right {\n    transform: translateX(100px);\n}\n\n.mouth {\n    width: 200px;\n    height: 200px;\n    position: absolute;\n    left: 50%;\n    top: 170px;\n    margin-left: -100px;\n}\n\n.mouth .up {\n    position: relative;\n    top: -20px;\n    z-index: 1;\n}\n\n.mouth .up .lip {\n    border: 3px solid black;\n    height: 30px;\n    width: 100px;\n    background: #ffe600;\n    border-top-color: transparent;\n    border-right-color: transparent;\n    position: relative;\n    position: absolute;\n    left: 50%;\n    margin-left: -50px;\n}\n\n.mouth .up .lip.left {\n    border-radius: 0 0 0 50px;\n    transform: rotate(-15deg) translateX(-53px);\n}\n\n.mouth .up .lip.right {\n    border-radius: 0 0 50px 0;\n    transform: rotate(15deg) translateX(53px);\n}\n\n.mouth .up .lip::before {\n    content: '';\n    display: block;\n    width: 7px;\n    height: 30px;\n    position: absolute;\n    bottom: 0;\n    background: #ffe600;\n}\n\n.mouth .up .lip.left::before {\n    right: -6px;\n}\n\n.mouth .up .lip.right::before {\n    left: -6px;\n}\n\n.mouth .down {\n    height: 180px;\n    position: absolute;\n    top: 5px;\n    width: 100%;\n    overflow: hidden;\n}\n\n.mouth .down .yuan1 {\n    border: 3px solid black;\n    width: 150px;\n    height: 1000px;\n    position: absolute;\n    bottom: 0;\n    left: 50%;\n    margin-left: -75px;\n    border-radius: 75px/300px;\n    background: #9b000a;\n    overflow: hidden;\n}\n\n.mouth .down .yuan1 .yuan2 {\n    width: 200px;\n    height: 300px;\n    background: #ff485f;\n    position: absolute;\n    bottom: -155px;\n    left: 50%;\n    margin-left: -100px;\n    border-radius: 100px;\n}\n\n.face {\n    position: absolute;\n    left: 50%;\n    border: 3px solid black;\n    width: 88px;\n    height: 88px;\n    top: 200px;\n    margin-left: -44px;\n    z-index: 3;\n}\n\n.face>img {\n    position: absolute;\n    top: 50%;\n    left: 50%;\n}\n\n.face.left {\n    transform: translateX(-180px);\n    background: #ff0000;\n    border-radius: 50%;\n}\n\n.face.left>img {\n    transform: rotateY(180deg);\n    transition: 0 0;\n}\n\n.face.right {\n    transform: translateX(180px);\n    background: #ff0000;\n    border-radius: 50%;\n}\n";
-var n = 1;
+})({"css.js":[function(require,module,exports) {
+"use strict";
 
-console.log(n);
-var id = setInterval();
-},{}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var string = ".skin *{box-sizing: border-box;margin: 0;padding: 0;}\n.skin *::before, .skin *::after{box-sizing: border-box;}\n\n.skin{\n  background: #ffe600;\n  min-height: 50vh;\n  position: relative;\n}\n.nose{\n  border: 10px solid black;\n  border-color: black transparent transparent;\n  border-bottom: none;\n  width: 0px;\n  height: 0px;\n  position: relative;\n  left: 50%;\n  top: 145px;\n  margin-left: -10px;\n  z-index: 10;\n}\n@keyframes wave{\n  0%{\n    transform: rotate(0deg);\n}\n  33%{\n    transform: rotate(5deg);\n}\n  66%{\n    transform: rotate(-5deg);\n}\n  100%{\n    transform: rotate(0deg);\n}\n\n}\n.nose:hover{\n  transform-origin: center bottom;\n  animation: wave 300ms infinite linear;\n}\n\n.yuan{\n  position: absolute;\n  width: 20px;\n  height: 6px;\n  top: -16px;\n  left: -10px;\n  border-radius: 10px 10px 0 0;\n  background: black;\n}\n\n.eye {\n  border: 2px solid #000;\n  width: 64px;\n  height: 64px;\n  position: absolute;\n  left: 50%;\n  top: 100px;\n  margin-left: -32px;\n  background: #2e2e2e;\n  border-radius: 50%;\n}\n.eye::before{\n  content: '';\n  display: block;\n  border: 3px solid #000;\n  width: 30px;\n  height: 30px;\n  background: #fff;\n  border-radius: 50%;\n  position: relative;\n  left: 4px;\n  top: 2px;\n}\n.eye.left{\n  transform: translateX(-100px);\n}\n.eye.right{\n  transform: translateX(100px);\n}\n.mouth{\n  width: 200px;\n  height: 200px;\n  position: absolute;\n  left: 50%;\n  top: 170px;\n  margin-left: -100px;\n}\n.mouth .up{\n  position: relative;\n  top: -20px;\n  z-index: 1;\n}\n.mouth .up .lip{\n  border: 3px solid black;\n  height: 30px;\n  width: 100px;\n  background: #ffe600;\n  border-top-color: transparent;\n  border-right-color: transparent;\n  position: relative;\n  position: absolute;\n  left: 50%;\n  margin-left: -50px;\n}\n.mouth .up .lip.left{\n  border-radius: 0 0 0 50px;\n  transform: rotate(-15deg) translateX(-53px);\n}\n.mouth .up .lip.right{\n  border-radius: 0 0 50px 0px;\n  transform: rotate(15deg) translateX(53px);\n}\n.mouth .up .lip::before{\n  content: '';\n  display: block;\n  width: 7px;\n  height: 30px;\n  position: absolute;\n  bottom: 0;\n  background: #ffe600;\n}\n.mouth .up .lip.left::before{\n  right: -6px;\n\n}\n.mouth .up .lip.right::before{\n  left: -6px;\n}\n.mouth .down{\n  height: 180px;\n  position: absolute;\n  top: 5px;\n  width: 100%;\n  overflow: hidden;\n}\n.mouth .down .yuan1{\n  border: 3px solid black;\n  width: 150px;\n  height: 1000px;\n  position: absolute;\n  bottom: 0;\n  left: 50%;\n  margin-left: -75px;\n  border-radius: 75px/300px;\n  background: #9b000a;\n  overflow: hidden;\n}\n.mouth .down .yuan1 .yuan2{\n  width: 200px;\n  height: 300px;\n  background: #ff485f;\n  position: absolute;\n  bottom: -155px;\n  left: 50%;\n  margin-left: -100px;\n  border-radius: 100px;\n\n}\n.face{\n  position: absolute;\n  left: 50%;\n  border: 3px solid black;\n  width: 88px;\n  height: 88px;\n  top: 200px;\n  margin-left: -44px;\n  z-index: 3;\n}\n.face > img{\n  position: absolute;\n  top: 50%;\n  left: 50%;\n}\n.face.left{\n  transform: translateX(-180px);\n  background: #ff0000;\n  border-radius: 50%;\n}\n.face.left > img{\n  transform: rotateY(180deg);\n  transform-origin: 0 0;\n}\n.face.right{\n  transform: translateX(180px);\n  background: #ff0000;\n  border-radius: 50%;\n}\n";
+exports.default = string;
+},{}],"test.js":[function(require,module,exports) {
+'use strict';
+
+var _css = require('./css.js');
+
+var _css2 = _interopRequireDefault(_css);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var player = {
+    id: undefined,
+    time: 100,
+    ui: {
+        demo: document.querySelector('#demo'),
+        demo2: document.querySelector('#demo2')
+    },
+    events: {
+        '#btnPause': 'pause',
+        '#btnPlay': 'play',
+        '#btnSlow': 'slow',
+        '#btnNormal': 'normal',
+        '#btnFast': 'fast'
+    },
+    n: 1,
+    init: function init() {
+        player.ui.demo.innerText = substring(0, player.n);
+        player.ui.demo2.innerHTML = substring(0, player.n);
+        player.bindEvents();
+        player.play();
+    },
+    bindEvents: function bindEvents() {
+        for (var key in player.events) {
+            if (player.events.hasOwnProperty(key)) {
+                var value = player.events[key]; // pause / play / slow
+                document.querySelector(key).onclick = player[value];
+            }
+        }
+    },
+    run: function run() {
+        player.n += 1;
+        if (player.n > _css2.default.length) {
+            window.clearInterval(player.id);
+            return;
+        }
+        player.ui.demo.innerText = substring(0, player.n);
+        player.ui.demo2.innerHTML = substring(0, player.n);
+        player.ui.demo.scrollTop = player.ui.demo.scrollHeight;
+    },
+    play: function play() {
+        window.clearInterval(player.id);
+        player.id = setInterval(player.run, player.time);
+    },
+    pause: function pause() {
+        window.clearInterval(player.id);
+    },
+    slow: function slow() {
+        player.pause();
+        player.time = 300;
+        player.play();
+    },
+    normal: function normal() {
+        player.pause();
+        player.time = 100;
+        player.play();
+    },
+    fast: function fast() {
+        player.pause();
+        player.time = 0;
+        player.play();
+    }
+};
+
+player.init();
+},{"./css.js":"css.js"}],"../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -138,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54690' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57409' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
