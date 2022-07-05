af4bd667ac168dd718166e4c33e5df2251db8c4c
yanglin <yanglin556@yahoo.com>
2022-07-05 15:44:53 +0800

    init

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '写代码啦',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
+};
