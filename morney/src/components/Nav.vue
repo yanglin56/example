313eed1f2349640bb9135905fbc20a2b0aa4f27d
yanglin <yanglin556@yahoo.com>
2022-08-26 19:32:31 +0800

    添加Nav 样式

--- morney/src/components/Nav.vue
+++ morney/src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-      <router-link to="/money">
+  <nav>
+      <router-link to="/money" class="item">
         <Icon name="money"/>
         记账
         </router-link>
-      ｜
-      <router-link to="/labels">
+      <router-link to="/labels" class="item">
           <Icon name="label"/>
         标签
         </router-link>
-      ｜
-      <router-link to="/statistics">
+      <router-link to="/statistics" class="item">
         <Icon name="statistics"/>
         统计
         </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -28,5 +26,21 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-   .nav {border: 1px solid red;}
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    }
 </style>
