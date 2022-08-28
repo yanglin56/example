32b338090412d2ec139ecc736da5bd925a58ffef
yanglin <yanglin556@yahoo.com>
2022-08-28 17:13:27 +0800

    init

--- morney/src/components/Money/Types.vue
+++ morney/src/components/Money/Types.vue
@@ -9,29 +9,20 @@
      </div>
 </template>
 
-<script>
+<script lang="ts" >
 import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name:'Types',
-  props: ['xxx'],
-  data() {
-    return {
-      type: '-' //'-'表示支出 '+'表示收入
-    }
-  },
-  mounted(){
-    console.log(this.xxx)
-  },
-  methods: {
-    selectType(type) { //type只能是 '-'和'+'中的一个
+@Component
+export default class Types extends Vue{
+  type = '-' //'-'表示支出 '+'表示收入
+  selectType(type: string) {
     if(type !== '-' && type !== '+') {
-      throw new Error('type is unknown')
+      throw new Error('type is unknown');
     }
-      this.type = type
+      this.type = type;
     }
-  }
-})
+}
 </script>
 
 <style lang="scss" scoped>
