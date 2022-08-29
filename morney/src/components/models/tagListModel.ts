89a51d46df6e5471dc66670743a0dd52fc3ae80e
yanglin <yanglin556@yahoo.com>
2022-08-29 20:11:35 +0800

    将tag的string改为 {id,name}

--- morney/src/components/models/tagListModel.ts
+++ morney/src/components/models/tagListModel.ts
@@ -1,8 +1,12 @@
 const localStorageKeyName ='tagList';
+type Tag = {
+  id: string;
+  name: string;
+}
 
 type TagListModel = {
-  data: string[]
-  fetch: () => string[]
+  data: Tag[]
+  fetch: () => Tag[]
   create:(name: string) => 'success' | 'duplicated' // success表示成功 duplicated表示name重复
   save: () => void
 } 
@@ -14,8 +18,10 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name: string) {
-    if (this.data.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push(name);
+    // this.data = [{id:'1', name:'1'},{id:'2', name:'2'}]
+    const names = this.data.map(item => item.name)
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id:name, name:name});
     this.save();
     return 'success';
   },
--- morney/src/router/index.ts
+++ morney/src/router/index.ts
@@ -1,30 +1,36 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
 import NotFound  from '@/views/NotFound.vue'
+import EditLabel from '@/views/EditLabel.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path:'/',
-    redirect:'/money'
+    path: '/',
+    redirect: '/money'
   },
   {
-    path:'/money',
-    component:Money
+    path: '/money',
+    component: Money
   },
   {
-    path:'/labels',
-    component:Labels
+    path: '/labels',
+    component: Labels
   },
   {
-    path:'/statistics',
-    component:Statistics
+    path: '/statistics',
+    component: Statistics
   },
+  {
+    path: '/labels/edit:id',
+    component: EditLabel
+  },
+
   { 
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
@@ -50,6 +56,6 @@ const routes = [
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
