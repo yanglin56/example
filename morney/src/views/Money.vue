ad2e033aa63d96b86a8ae1eb411f57f91deabd94
yanglin <yanglin556@yahoo.com>
2022-08-29 17:13:28 +0800

    展示和创建标签

--- morney/src/views/Money.vue
+++ morney/src/views/Money.vue
@@ -14,15 +14,17 @@ import Types from '../components/Money/Types.vue';
 import Notes from '../components/Money/Notes.vue';
 import Tags from '../components/Money/Tags.vue';
 import { Component, Watch} from 'vue-property-decorator';
-import model from '@/model';
+import recordListModel from '@/components/models/recordListModel';
+import tagListModel from '@/components/models/tagListModel';
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({
   components: { Tags, Notes, Types, NumberPad },
 })
 export default class Money extends Vue {
-  tags = ['衣','食','住','行'];
+  tags = tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [],notes:'', type: '-', amount: 0
@@ -35,7 +37,7 @@ export default class Money extends Vue {
      this.record.notes = value;
   }
   saveRecord(){
-    const record2:RecordItem = model.clone(this.record);
+    const record2:RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
@@ -43,7 +45,7 @@ export default class Money extends Vue {
 
   @Watch('recordList') 
   onRecordListChange() {
-      model.save(this.recordList);
+    recordListModel.save(this.recordList);
     }
 }
 </script>
