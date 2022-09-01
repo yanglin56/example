917b7e37b0e468b0ecb77d17db0f89e667f957bb
yanglin <yanglin556@yahoo.com>
2022-09-01 12:19:11 +0800

    修复 value 被赋值的问题

--- morney/src/views/Money.vue
+++ morney/src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
     <Layout classPrefix="layout">
+      {{record}}
      <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
      <Types :value.sync="record.type"/>
      <div class="notes">
