d77d85ddad0ccd4d5491f68d42852e6ef7856bf9
yanglin <yanglin556@yahoo.com>
2022-09-01 17:42:09 +0800

    可以编辑和删除标签

--- morney/src/components/models/tagListModel.ts
+++ morney/src/components/models/tagListModel.ts
@@ -7,7 +7,9 @@ type Tag = {
 type TagListModel = {
   data: Tag[]
   fetch: () => Tag[]
-  create:(name: string) => 'success' | 'duplicated' // success表示成功 duplicated表示name重复
+  create: (name: string) => 'success' | 'duplicated' // success表示成功 duplicated表示name重复
+  updated: (id: string,name: string) => 'success' | 'not found' | 'duplicated' 
+  remove: (id: string)=>boolean
   save: () => void
 } 
 
@@ -24,6 +26,34 @@ const tagListModel: TagListModel = {
     this.data.push({id:name, name:name});
     this.save();
     return 'success';
+  },
+  updated (id:string, name:string) {
+    const idList =this.data.map(item => item.id);
+    if(idList.indexOf(id) >=0) {
+      const names =this.data.map(item => item.name);
+      if(names.indexOf(name)>=0){
+        return 'duplicated';
+      }else{
+        const tag = this.data.filter(item => item.id ===id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'not found'
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for(let i=0;i< this.data.length; i++){
+      if (this.data[i].id === id){
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
