ea813e82bb67acee38bd008ab1acea3196aebfda
yanglin <yanglin556@yahoo.com>
2022-09-03 17:12:10 +0800

    Tags.vue 使用 Vuex

--- morney/src/components/Money/Tags.vue
+++ morney/src/components/Money/Tags.vue
@@ -20,14 +20,17 @@
   @Component({
     computed: {
       tagList() {
-      //  return this.$store.fetchTags();
-      return []
+       return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
-
     selectedTags: string[] = [];
+
+    created () {
+      this.$store.commit('fetchTags');
+    }
+
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
       if (index >= 0) {
@@ -40,7 +43,7 @@
     create() {
       const name = window.prompt('请输入标签名');
       if (!name) { return window.alert('标签名不能为空'); }
-      // store.createTag(name);
+      this.$store.commit('createTag',name);
     }
   }
 </script>
--- morney/src/store/index.ts
+++ morney/src/store/index.ts
@@ -1,4 +1,5 @@
 import clone from '@/lib/clone';
+import createId from '@/lib/createId';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -7,7 +8,8 @@ Vue.use(Vuex); // 把store 绑到 Vue.prototype.$store = store
 
 const store = new Vuex.Store({
   state: { //data
-    recordList: [] as RecordItem[]
+    recordList: [] as RecordItem[],
+    tagList:[] as Tag[],
   },
   mutations: { //methods
     fetchRecords(state) {
@@ -18,13 +20,31 @@ const store = new Vuex.Store({
       record2.createdAt = new Date();
       state.recordList.push(record2);
       store.commit('saveRecords')
-      // recordStore.saveRecords();
     },
     saveRecords(state) {
       window.localStorage.setItem('recordList',
        JSON.stringify(state.recordList));
     },
+    fetchTags(state) {
+      return state.tagList  = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+    },
+    createTag(state,name: string) {
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签名重复了');
+        return 'duplicated';
+      }
+      const id = createId().toString();
+      state.tagList.push({id, name: name});
+      store.commit('saveTags');
+      window.alert('添加成功');
+      return 'success';
+    },
+    saveTags(state) {
+      window.localStorage.setItem('tagList', 
+        JSON.stringify(state.tagList));
+    }
   }
-});
+}); 
 
 export default store;
