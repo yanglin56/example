2e878107cd3f016aec773b4b01e1b7a39c112cf7
yanglin <yanglin556@yahoo.com>
2022-09-04 21:52:14 +0800

    抽离 Tabs.vue 使用在Statistics.vue 里面

--- morney/src/components/Money/Types.vue
+++ morney/src/components/Money/Types.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'"
+      <li :class="{[classPrefix+'-item']:classPrefix, selected: value === '-'}"
           @click="selectType('-')">支出
       </li>
-      <li :class="value === '+' && 'selected'"
+      <li :class="{[classPrefix+'-item']:classPrefix, selected: value === '+'}"
           @click="selectType('+')">收入
       </li>
     </ul>
@@ -14,9 +14,11 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component, Prop, Watch} from 'vue-property-decorator';
+  
   @Component
   export default class Types extends Vue {
-    @Prop() readonly value!: string;
+    @Prop(String) readonly value!: string;
+    @Prop(String) classPrefix?: string;
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown');
